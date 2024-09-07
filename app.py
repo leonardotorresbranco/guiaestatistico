a69a3b33bf9ffd004ff6800a7b5301a6e23a6bb2
leonardotorresbranco <leonardotorresbranco@gmail.com>
2024-09-06 23:15:29 -0300

    TED

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ from collections import defaultdict, Counter
 
 app = Flask(__name__)
 
-# Carregar dados do arquivo JSON
-with open('qbank.qbank_prebuilt_tests_porinst.json', 'r', encoding='utf-8') as f:
+with open('testeTED.json', 'r', encoding='utf-8') as f:
     data = json.load(f)
 
 @app.route('/')
@@ -31,8 +30,8 @@ def filter_data():
     institution_id = int(request_data['institution'])
     start_year = int(request_data['startYear'])
     end_year = int(request_data['endYear'])
+    
 
-    # Separar arrays de tags e parentTags
     tags_data = []
     parent_tags_data = []
 
@@ -40,42 +39,40 @@ def filter_data():
         if test.get('institutionId') == institution_id and start_year <= test.get('year') <= end_year:
             tags_data.extend(test.get('tags', []))
             parent_tags_data.extend(test.get('parentTags', []))
-
-    # Função auxiliar para extrair o ID da tag
+    print (len(tags_data))
     def extract_oid(tag):
         return tag['_id']['$oid']
 
-    # Criar contadores de tags
     tag_count = Counter()
 
-    # Contar tags primárias e suas frequências
     for tag in tags_data:
         tag_id = extract_oid(tag)
         tag_count[tag_id] += 1
-        parent_id = tag.get('parentId', {})
-        if isinstance(parent_id, dict):
-            parent_id = parent_id.get('$oid')
-            if parent_id:
-                tag_count[parent_id] += 0  # Garante que o parent_id seja contado mesmo que não apareça na lista de tags
+        # parent_id = tag.get('parentId', {})
+        # if isinstance(parent_id, dict):
+        #     parent_id = parent_id.get('$oid')
+        #     if parent_id:
+        #         tag_count[parent_id] += 0  # Garante que o parent_id seja contado mesmo que não apareça na lista de tags
 
     for tag in parent_tags_data:
         tag_id = extract_oid(tag)
         tag_count[tag_id] += 1
-        parent_id = tag.get('parentId', {})
-        if isinstance(parent_id, dict):
-            parent_id = parent_id.get('$oid')
-            if parent_id:
-                tag_count[parent_id] += 0  # Garante que o parent_id seja contado mesmo que não apareça na lista de tags
+        # parent_id = tag.get('parentId', {})
+        # if isinstance(parent_id, dict):
+        #     parent_id = parent_id.get('$oid')
+        #     if parent_id:
+        #         tag_count[parent_id] += 0  # Garante que o parent_id seja contado mesmo que não apareça na lista de tags
 
     
-    
+    with open('counter.json', 'w', encoding='utf-8') as f:
+        json.dump(tag_count, f, ensure_ascii=False, indent=4)
 
-    # Criar a árvore de tags
+
     tag_tree = {}
     tag_frequencies = defaultdict(float)
 
-    # Processar o array de tags para criar a árvore inicial
     for tag in tags_data:
+
         tag_id = extract_oid(tag)
         parent_id = tag.get('parentId', {})
         if isinstance(parent_id, dict):
@@ -85,10 +82,9 @@ def filter_data():
             'name': tag['name'],
             'parentId': parent_id,
             'children': [],
-            'absoluteValue': tag_count[tag_id] 
+            'absoluteValue': tag_count[tag_id],
         }
 
-    # Adicionar informações dos parentTags à árvore de tags
     for parent_tag in parent_tags_data:
         tag_id = extract_oid(parent_tag)
         parent_id = parent_tag.get('parentId', {})
@@ -100,10 +96,9 @@ def filter_data():
                 'name': parent_tag['name'],
                 'parentId': parent_id,
                 'children': [],
-                'absoluteValue': tag_count[tag_id] 
+                'absoluteValue': tag_count[tag_id],
             }
 
-    # Construir a lista de operações para adicionar filhos
     operations = []
 
     for tag_id, tag_info in tag_tree.items():
@@ -111,7 +106,6 @@ def filter_data():
         if parent_id:
             operations.append((parent_id, tag_info))
 
-    # Aplicar as operações de adição de filhos
     for parent_id, tag_info in operations:
         if parent_id in tag_tree:
             tag_tree[parent_id]['children'].append(tag_info)
@@ -122,50 +116,48 @@ def filter_data():
 
     def calculate_frequencies(node, sibling_count):
         if 'children' in node and node['children']:
-            # Calcula o total de contagem dos filhos para a tag atual
             child_count = sum(tag_count[child['_id']] for child in node['children'])
 
-            # Se a soma das contagens das tags filhas for menor que a contagem da tag mãe
             if child_count < tag_count[node['_id']]:
-                # Calcula a diferença e a frequência para a nova tag "Outras"
                 other_tag_count = tag_count[node['_id']] - child_count
                 other_tag_frequency = other_tag_count / tag_count[node['_id']] if sibling_count > 0 else 0
 
-                # Cria a nova tag "Outras"
                 other_tag = {
                     '_id': 'other_' + node['_id'],  # ID único para a tag "Outras"
                     'name': f'Outras tags sobre {node["name"]}',
                     'parentId': node['_id'],
                     'children': [],
                     'absoluteValue': other_tag_count,
+                    
                     'frequency': other_tag_frequency
+                    
                 }
 
-                # Adiciona a nova tag "Outras" à lista de filhos
                 node['children'].append(other_tag)
 
-                # Atualiza a contagem dos filhos após adicionar a nova tag "Outras"
                 child_count = sum(tag_count[child['_id']] for child in node['children'])
 
-            # Atualiza a frequência de cada filho
             for child in node['children']:
+                #child['symbol'] = node['symbol']
                 if sibling_count > 0:
                     child['frequency'] = tag_count[child['_id']] / sibling_count
                 else:
                     child['frequency'] = 0
                     print("epa")
                 calculate_frequencies(child, tag_count[node['_id']])
-        # Calcula a frequência da tag atual em relação à soma das tags irmãs (sibling_count)
         if sibling_count > 0 and tag_count[node['_id']] > 0:
             node['frequency'] = tag_count[node['_id']] / sibling_count
         else:
             node['frequency'] = node['absoluteValue'] / sibling_count
 
-    # Calcular a frequência das tags primárias
     primary_tags = [tag for tag_id, tag in tag_tree.items() if tag['parentId'] is None]
     total_primary_count = sum(tag_count[tag['_id']] for tag in primary_tags)
 
     for tag in primary_tags:
+        
+       
+            
+
         if total_primary_count > 0:
             tag['frequency'] = tag_count[tag['_id']] / total_primary_count
         else:
@@ -173,19 +165,26 @@ def filter_data():
         calculate_frequencies(tag, total_primary_count)
 
     
+    primary_tags[0]['frequency'] = 1
+    primary_tags[0]['name'] = "Dermatologia"
+
+    for tag in primary_tags:
+        if tag['name'] == "Dermatologia":
+            print(tag['name']) 
+            primary_tags.clear()
+            primary_tags.append(tag) 
+            break
 
 
 
 
-
-    # Estruturar os dados para D3.js
     hierarchical_data = {
         'name': 'Tags',
         'children': primary_tags
     }
     
     #with open('hierarchical_data.json', 'w', encoding='utf-8') as f:
-     #   json.dump(hierarchical_data, f, ensure_ascii=False, indent=4)
+    #    json.dump(hierarchical_data, f, ensure_ascii=False, indent=4)
 
     return jsonify(hierarchical_data)
 
